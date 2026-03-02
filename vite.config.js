f4efdeeddceb4eda2b5f9f43722eff1f326b523e
unknown <you@example.com>
2026-03-02 14:42:14 +0600

    add base repo name

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/40_coutries_flag_react_project_using_api_daisyUI/',
   plugins: [react()],
 })
